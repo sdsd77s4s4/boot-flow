7a99411a9c2d437506dc0f9b4a75a9413110022d
perfectplaytv <graficahome@zoho.com>
2025-07-17 19:33:19 -0300

    confirmar

---
+++ src/pages/AdminIPTV.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminIPTV() {
+  return (
+    <div>
+      <h1>Sistema IPTV</h1>
+      <p>Página de gerenciamento do sistema IPTV.</p>
+    </div>
+  );
+} 
---
+++ src/pages/AdminUsers.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminUsers() {
+  return (
+    <div>
+      <h1>Usuários</h1>
+      <p>Página de gerenciamento de usuários do administrador.</p>
+    </div>
+  );
+} 
