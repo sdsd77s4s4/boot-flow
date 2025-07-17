5f870d82574c716d4d95c21e43aae4cb41a9d9b2
perfectplaytv <graficahome@zoho.com>
2025-07-17 19:33:41 -0300

    confirmar

---
+++ src/pages/AdminAnalytics.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminAnalytics() {
+  return (
+    <div>
+      <h1>Analytics</h1>
+      <p>Página de análise de dados e métricas.</p>
+    </div>
+  );
+} 
