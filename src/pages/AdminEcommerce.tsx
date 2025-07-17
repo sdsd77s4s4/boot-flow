e2e2ff8a01cdf4b0ccec6170938c66069a51565c
perfectplaytv <graficahome@zoho.com>
2025-07-17 19:33:32 -0300

    confirmar

---
+++ src/pages/AdminEcommerce.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AdminEcommerce() {
+  return (
+    <div>
+      <h1>E-commerce</h1>
+      <p>Página de gerenciamento do e-commerce.</p>
+    </div>
+  );
+} 
