401920d18b379be56a10663a98ef5732232cfa1e
perfectplaytv <graficahome@zoho.com>
2025-07-19 19:50:42 -0300

    confirmar

--- src/pages/AdminResellers.tsx
+++ src/pages/AdminResellers.tsx
@@ -543,7 +543,9 @@ export default function AdminResellers() {
                       </>
                     ) : (
                       <>
-                        <CheckCircle className="w-4 h-4 mr-2" />
+                        <svg className="w-4 h-4 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4" />
+                        </svg>
                         Salvar
                       </>
                     )}
