99dcfa14f88cf972df5d648289b4eabed22f07cf
perfectplaytv <graficahome@zoho.com>
2025-07-21 21:42:32 -0300

    confirmar

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
+console.log('SUPABASE_URL:', process.env.SUPABASE_URL);
+console.log('SUPABASE_ANON_KEY:', process.env.SUPABASE_ANON_KEY);
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
 
